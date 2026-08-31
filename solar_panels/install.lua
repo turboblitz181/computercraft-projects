91b0aca554196953b6e5f8188d4c0cdb80d3ad17
turboblitz181 <joepvandoorn@live.nl>
2026-08-31 11:50:24 +0200

    Add startup buttons for main and sector installations

--- solar_panels/install.lua
+++ solar_panels/install.lua
@@ -1,1 +1,50 @@
+main_startup_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/main_computer/startup.lua?token=GHSAT0AAAAAAEAEAEUOKOYPKJGTWT46M6VW2UVJURA"
+sector_1_startup_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/sector_1/startup.lua?token=GHSAT0AAAAAAEAEAEUOXEDP2G5CU4R2BQVK2UVJVFQ"
 
+if not fs.exists("basalt") then
+    shell.run("wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua")
+end
+
+local basalt = require("basalt")
+
+local main = basalt.getMainFrame()
+
+
+main:addButton():setText("main startup"):setPosition(2, 2):onClick(function(self)
+        local request = http.get(main_startup_url)
+        if not request then
+            self:setText("error installing!")
+            return
+        end
+        local file = fs.open("startup.lua", "w")
+        file.write(request.readAll())
+        file.close()
+        request.close()
+        self:setText("installed!")
+    end)
+
+main:addButton():setText("sector1 startup"):setPosition(2, 4):onClick(function(self)
+        local request = http.get(sector_1_startup_url)
+        if not request then
+            self:setText("error installing!")
+            return
+        end
+        local file = fs.open("startup.lua", "w")
+        file.write(request.readAll())
+        file.close()
+        request.close()
+        self:setText("installed!")
+    end)
+
+main:addButton():setText("sector 2 startup"):setPosition(2, 6):onClick(function(self)
+        local request = http.get(sector_2_startup_url)
+        if not request then
+            self:setText("error installing!")
+            return
+        end
+        local file = fs.open("startup.lua", "w")
+        file.write(request.readAll())
+        file.close()
+        request.close()
+        self:setText("installed!")
+    end)
