eea3bb76f2042a65ff7a46331e8927f3ba150287
turboblitz181 <joepvandoorn@live.nl>
2026-08-31 11:52:36 +0200

    Remove Basalt installation check from install.lua
    Removed the check for Basalt installation and its wget command.

--- solar_panels/install.lua
+++ solar_panels/install.lua
@@ -1,10 +1,6 @@
 main_startup_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/main_computer/startup.lua?token=GHSAT0AAAAAAEAEAEUOKOYPKJGTWT46M6VW2UVJURA"
 sector_1_startup_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/sector_1/startup.lua?token=GHSAT0AAAAAAEAEAEUOXEDP2G5CU4R2BQVK2UVJVFQ"
 
-if not fs.exists("basalt") then
-    shell.run("wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua")
-end
-
 local basalt = require("basalt")
 
 local main = basalt.getMainFrame()
