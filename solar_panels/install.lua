0a0cbe3a55b56c3fa74f243ce035f878b7aee493
turboblitz181 <joepvandoorn@live.nl>
2026-08-31 12:00:07 +0200

    Add update URL for solar panels installation

--- solar_panels/install.lua
+++ solar_panels/install.lua
@@ -1,3 +1,2 @@
-main_startup_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/main_computer/startup.lua?token=GHSAT0AAAAAAEAEAEUOKOYPKJGTWT46M6VW2UVJURA"
-sector_1_startup_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/sector_1/startup.lua?token=GHSAT0AAAAAAEAEAEUOXEDP2G5CU4R2BQVK2UVJVFQ"
+update_url = "https://raw.githubusercontent.com/turboblitz181/computercraft-projects/refs/heads/main/solar_panels/update.lua"
 
